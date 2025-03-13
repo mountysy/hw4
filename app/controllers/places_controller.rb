4b5ba04ed7bd97fc6ac1e10a20c612cf7cd54025
Shan Yan <shan.yan@kellogg.northwestern.edu>
2025-03-13 22:15:48 +0000

    HW4 stage1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,6 +7,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @entries = Entry.where({ "place_id" => @place["id"] })
+    @user = User.find_by({"id" => session["user_id"]})
   end
 
   def new
